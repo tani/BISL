198e27161f1b2bed0aa1d4ade91864b75c0921ad
TANIGUCHI Masaya <mew@cat.ovh>
2015-10-09 14:09:24 +0900

    Mix

---
+++ mix.exs
@@ -0,0 +1,13 @@
+defmodule BISL.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :bisl,
+     version: "0.0.1",
+     elixir: "~> 1.1",
+		 escript: [main_module: BISL],
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod]
+  end
+
+end
